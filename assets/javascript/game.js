6cd781d4523378a1355b9c1ef9df7b430fbab64e
SkyNet <nme312.noreply.github.com>
2018-04-17 19:49:49 -0500

    created all variables and array, generating random number as array index, added key event function for user input

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+var wordList = [
+"Top Gun", 
+"Back To The Future", 
+"Ferris Bueller's Day Off", 
+"Butch Cassidy and The Sundance Kid", 
+"The Fugitive",
+"Scream",
+"Forest Gump",
+"Star Wars",
+"Norbit",
+"The Sandlot",
+"Little Rascals",
+"The God Father",
+"Independence Day",
+"E.T.",
+"Titanic",
+"Avatar",
+"Jurrassic Park",
+"Lion King",
+"The Jungle Book",
+"Black Panther",
+"Captain America: Civil War",
+"DeadPool",
+"Mean Girls",
+"Chronicle",
+"The Dark Knight",
+"The Matrix",
+"The Room",
+"Cinderella Man",
+"Footloose",
+"Lords of Dogtown",
+"GhostBusters"
+]
+
+var conChk = 0;
+
+var userGuess = document.getElementById("userGuess");
+var pastGuesses;
+var guessLimit = 10;
+
+var currentWord = document.getElementById("currentWord");
+var randomWord = (Math.floor(Math.random() * wordList.length));
+
+currentWord.textContent = wordList[randomWord];
+
+document.onkeyup =function(event) {
+    var currentGuess = event.key;
+    userGuess.textContent = currentGuess;
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(currentWord);
+console.log(userGuess);
+console.log(conChk);
+
+
